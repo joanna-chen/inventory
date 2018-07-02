3f2dde58bed7d9d570f2a9028a79a5ef5bfe6687
joanna-chen <joannachen.x@gmail.com>
2018-07-01 17:54:15 -0700

    initial ios project

---
+++ Moku-iOS/Moku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moku
+//
+//  Created by Joanna Chen on 2018-07-01.
+//  Copyright © 2018 Moku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
